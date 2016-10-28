304f9ed5892520adb20ccfb892b532d2c6f82dbe
李娜 <18840861676@163.com>
2016-10-28 17:05:26 +0800

    Initial Commit

---
+++ LYC_UseTag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYC_UseTag
+//
+//  Created by 李娜 on 2016/10/28.
+//  Copyright © 2016年 李娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYC_UseTag/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYC_UseTag
+//
+//  Created by 李娜 on 2016/10/28.
+//  Copyright © 2016年 李娜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
